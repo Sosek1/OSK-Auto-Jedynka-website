9ea0575ca8fe981f1d0324417cc58320da70f3d0
Bartek Sosin <bscode03@gmail.com>
2021-02-25 17:58:21 +0100

    finish na

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,14 +1,15 @@
 const burger = document.querySelector('.burger');
 const list = document.querySelector('.list');
-let showMenu = false;
+const links = document.querySelectorAll('.list li');
+const bars = document.querySelector('.fa-bars'); 
+const times = document.querySelector	('.fa-times'); 
+
 burger.addEventListener('click', () => {
-	if (!showMenu) {
-		list.classList.add('showNav');
-		showMenu = true;
-	}
-	else {
-		list.classList.remove('showNav');
-		showMenu = false;
-	}
+	times.classList.toggle('appear');
+	bars.classList.toggle('dissapear');
+	list.classList.toggle('showNav');
+	links.forEach(link => {
+		link.classList.toggle('fade');
+	})
 });
 
