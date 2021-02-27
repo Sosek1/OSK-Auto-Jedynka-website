06396dcda4150a8244f906e028a23359a2934d60
Bartek Sosin <bscode03@gmail.com>
2021-02-27 18:13:00 +0100

    fix menu scrolling

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,9 +2,11 @@ const burger = document.querySelector('.burger');
 const list = document.querySelector('.list');
 const links = document.querySelectorAll('.list li');
 const bars = document.querySelector('.fa-bars'); 
-const times = document.querySelector	('.fa-times'); 
+const times = document.querySelector('.fa-times'); 
+const body = document.querySelector('body');
 
 burger.addEventListener('click', () => {
+	body.classList.toggle('changeOverflow');
 	times.classList.toggle('appear');
 	bars.classList.toggle('dissapear');
 	list.classList.toggle('showNav');
