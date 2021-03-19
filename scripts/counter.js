c74aac605846558ebc030977866e3607f1c49621
Bartek Sosin <bscode03@gmail.com>
2021-03-19 18:01:45 +0100

    changes

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,35 +1,37 @@
-const counter1 = document.querySelector('.eighty');
+const counter1 = document.querySelector('.number2');
 const counter2 = document.querySelector('.thousand');
-const counter3 = document.querySelector('.thirteen');
-const numberContainer = document.querySelector('.numbers');
+const counter3 = document.querySelector('.number3');
+const numberContainer = document.querySelector('.trick');
 counter1.innerText='0';
 counter2.innerText='0';
 counter3.innerText='0';
 
 function counter(){
     const updateCounter = () => {
-                    const target = +counter1.getAttribute('data-target');
-                    const c = +counter1.innerText;
-                    const target2 = +counter2.getAttribute('data-target');
-                    const c2 = +counter2.innerText;
-                    const target3 = +counter3.getAttribute('data-target');
-                    const c3 = +counter3.innerText;
+                    let target = +counter1.getAttribute('data-target');
+                    let c = +counter1.innerText;
+                    console.log(c);
+                    let target2 = +counter2.getAttribute('data-target');
+                    let c2 = +counter2.innerText;
+                    let target3 = +counter3.getAttribute('data-target');
+                    let c3 = +counter3.innerText;
                     if(c < target){
-                        counter1.innerText = c + 100;
+                        counter1.innerText = c + 1;
                         setTimeout(updateCounter, 100); 
                      }else{
                          counter1.innerText = target;
                      }
                     if(c2 < target2){
-                       counter2.innerText = c + 10000;
-                       setTimeout(updateCounter, 100); 
+                       counter2.innerText = c2 + 10;
+                       setTimeout(updateCounter, 1000); 
+                     
                     }else{
                         counter2.innerText = target2;
                     }
 
                     if(c3 < target3){
-                        counter3.innerText = c + 100;
-                       setTimeout(updateCounter, 100); 
+                        counter3.innerText = c3 + 1;
+                       setTimeout(updateCounter, 1000); 
                     }else{
                         counter3.innerText = target3;
                     }
@@ -37,13 +39,16 @@ function counter(){
         updateCounter();
     }
 
+
+
 window.addEventListener('scroll', () => {
     const scrolled = window.scrollY;
-    const elementPosition = numberContainer.offsetTop;
-    if(scrolled == elementPosition){
+    let elementPosition = numberContainer.offsetTop;
+    if(scrolled >= elementPosition){
         counter();
     }
 })
+
 
 
 
