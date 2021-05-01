52d37fd6ea5f1294e5ee8be51fcad1ea51c50a74
Bartek Sosin <bscode03@gmail.com>
2021-05-01 18:09:10 +0200

    fixing map and menu

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,7 @@
 function initMap() {
     var location = {
-        lat: 50.009790,
-        lng: 19.874990
+        lat: 50.02049638086401,
+        lng: 20.04963854094953
     };
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
